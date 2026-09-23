6ecc220eddc80383c09d2c8ea438d0ff1ad6811f
Eswlnk <1525566427@qq.com>
2026-09-23 16:09:56 +0800

    feat(r7): export forecast-native model (#4)

--- model/__init__.py
+++ model/__init__.py
@@ -1,3 +1,13 @@
 from .urban_pidit_r2 import UrbanPiDiTR2, R2Output
+from .weather_forecaster_r7 import NativeAtmosForecaster, R7ForecastOutput
 from .state import WeatherState, ReasoningAction, ReasoningTrace
-__all__=["UrbanPiDiTR2","R2Output","WeatherState","ReasoningAction","ReasoningTrace"]
+
+__all__=[
+    "UrbanPiDiTR2",
+    "R2Output",
+    "NativeAtmosForecaster",
+    "R7ForecastOutput",
+    "WeatherState",
+    "ReasoningAction",
+    "ReasoningTrace",
+]
