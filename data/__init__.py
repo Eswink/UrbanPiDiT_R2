01697bfc00a33136d7371aff8eb125c5c4956f00
Eswlnk <1525566427@qq.com>
2026-09-23 16:10:00 +0800

    feat(r7): export forecast-native data interfaces (#4)

--- data/__init__.py
+++ data/__init__.py
@@ -1,5 +1,23 @@
-from .schema import validate_sample, REQUIRED_KEYS
+from .schema import (
+    validate_sample,
+    validate_forecast_sample,
+    REQUIRED_KEYS,
+    R7_FORECAST_REQUIRED_KEYS,
+)
 from .synthetic import SyntheticR2Dataset
+from .synthetic_atmos import SyntheticAtmosDataset
 from .multiscale_dataset import ManifestNPZDataset, R2DataModule
+from .r7_dataset import ManifestAtmosNPZDataset, R7ForecastDataModule
 
-__all__ = ["validate_sample", "REQUIRED_KEYS", "SyntheticR2Dataset", "ManifestNPZDataset", "R2DataModule"]
+__all__ = [
+    "validate_sample",
+    "validate_forecast_sample",
+    "REQUIRED_KEYS",
+    "R7_FORECAST_REQUIRED_KEYS",
+    "SyntheticR2Dataset",
+    "SyntheticAtmosDataset",
+    "ManifestNPZDataset",
+    "ManifestAtmosNPZDataset",
+    "R2DataModule",
+    "R7ForecastDataModule",
+]
