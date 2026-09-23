22dbf30a959d84d2717ce27366268fed21c31228
Eswlnk <1525566427@qq.com>
2026-09-23 16:13:33 +0800

    feat(r7): export generic recursive weather baseline (#5)

--- model/__init__.py
+++ model/__init__.py
@@ -1,5 +1,9 @@
 from .urban_pidit_r2 import UrbanPiDiTR2, R2Output
 from .weather_forecaster_r7 import NativeAtmosForecaster, R7ForecastOutput
+from .recursive_weather_r7 import (
+    GenericRecursiveWeatherForecaster,
+    RecursiveForecastOutput,
+)
 from .state import WeatherState, ReasoningAction, ReasoningTrace
 
 __all__=[
@@ -7,6 +11,8 @@ __all__=[
     "R2Output",
     "NativeAtmosForecaster",
     "R7ForecastOutput",
+    "GenericRecursiveWeatherForecaster",
+    "RecursiveForecastOutput",
     "WeatherState",
     "ReasoningAction",
     "ReasoningTrace",
